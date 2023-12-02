e6e9ce8ac61fcceb08b9b2200da379dfad75c7e6
Ondřej Polanecký <ondrej.polanecky@cdn77.com>
2023-12-02 15:30:09 +0100

    krasa parada

---
+++ velkejondra/Day-2/part2/Program.fs
@@ -0,0 +1,36 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+open System.Text.RegularExpressions
+let lines = System.IO.File.ReadAllLines("day2.txt")
+
+let maxRed = 12
+let maxGreen = 13
+let maxBlue = 14
+
+let parsedLines = 
+    lines
+    |> Array.map (fun line -> line[line.IndexOf(": ")+2..])
+
+
+let getMaxMatch(matchCollection:MatchCollection) =
+    matchCollection
+    |> Seq.map(fun m ->int (m.Groups.Item(1).Value))
+    |> Seq.maxBy(fun value -> int value)
+
+let max_per_color =
+    parsedLines
+    |> Array.map(fun line -> [|Regex.Matches(line, @"(\d+) red");
+                                       Regex.Matches(line, @"(\d+) green");
+                                       Regex.Matches(line, @"(\d+) blue")|])
+    |> Array.map(fun (arr) -> arr|> Array.map(fun m -> getMaxMatch(m)))
+
+
+let sum = 
+    max_per_color
+    |> Array.map(fun arr -> arr |> Array.fold(fun acc value -> acc * value) 1)
+    |> Array.sum
+
+
+
+
+printfn "%A" sum
--- velkejondra/readme.md
+++ velkejondra/readme.md
@@ -2,3 +2,4 @@
 
 ## Day 1
 - fortran - DONE
+- F# - DONE
